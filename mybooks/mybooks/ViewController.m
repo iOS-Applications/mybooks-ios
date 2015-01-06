09c863c65b77970ce4b405d36db6c6481de3ce76
wangdengwu <dengwu.wang@gmail.com>
2015-01-06 08:35:47 +0800

    init project

---
+++ mybooks/mybooks/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mybooks
+//
+//  Created by Dengwu Wang on 1/6/15.
+//  Copyright (c) 2015 Thoughtworks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ mybooks/mybooks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mybooks
+//
+//  Created by Dengwu Wang on 1/6/15.
+//  Copyright (c) 2015 Thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
